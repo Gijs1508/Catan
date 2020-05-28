bf4e0f049ccc8705cb3a5f2c12dc0dca6184c75f
Jeroen <jegbol01@gmail.com>
2020-05-28 17:43:05 +0200

    Added simple functionality to lobbyView
    Switching to another FXML doesn't change the window size

--- src/main/java/org/catan/App.java
+++ src/main/java/org/catan/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("Views/chatView"));
+        scene = new Scene(loadFXML("Views/lobbyView"));
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/org/catan/Controller/LobbySchermController.java
@@ -0,0 +1,54 @@
+package org.catan.Controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.Pane;
+import javafx.scene.text.Text;
+import org.catan.App;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LobbySchermController implements Initializable {
+
+    private int aantalSpelers;
+    private int minimumAantalSpelers;
+
+    @FXML Pane player1pane;
+    @FXML Pane player2pane;
+    @FXML Pane player3pane;
+    @FXML Pane player4pane;
+    @FXML Text player1name;
+    @FXML Text player2name;
+    @FXML Text player3name;
+    @FXML Text player4name;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        player1pane.setVisible(false);
+        player2pane.setVisible(false);
+        player3pane.setVisible(false);
+        player4pane.setVisible(false);
+    }
+
+    @FXML
+    private void startGame() {
+        // Deze functionaliteit zal verplaatst en uitgebreid worden (voor wanneer een speler de sessie betreed).
+        player1pane.setVisible(true);
+        player1name.setText("Jeroen");
+    }
+
+    @FXML
+    private void leaveGame() throws IOException {
+        App.setRoot("Views/joinView");
+    }
+
+    private void genoegAantalSpelers() {
+
+    }
+
+    private void startTimer() {
+
+    }
+}
