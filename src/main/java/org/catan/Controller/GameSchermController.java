b04c5d8c8733edfaadd07ae5d11d502de998da51
Jeroen <jegbol01@gmail.com>
2020-05-30 13:54:04 +0200

    Added gameschermcontroller

---
+++ src/main/java/org/catan/Controller/GameSchermController.java
@@ -0,0 +1,55 @@
+package org.catan.Controller;
+
+//import Model.Spel;
+//import Model.Spelbord;
+//import Model.Speler;
+
+import javafx.fxml.FXML;
+import javafx.scene.input.MouseEvent;
+
+public class GameSchermController {
+
+    private int aantalSpelers;
+//    private Spelbord spelbord;
+//    private Spel spel;
+
+    public GameSchermController() {
+
+    }
+
+    private void keyHandler() {
+
+    }
+
+    private void timer() {
+
+    }
+
+    private void handelMetBank() {
+
+    }
+
+    private void handelMetSpelers() {
+
+    }
+
+    private void gebruikRidderkaart() {
+
+    }
+
+    @FXML
+    public void buildSettlement(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void showVertex(MouseEvent mouseEvent) {
+    }
+
+    @FXML
+    public void hideVertex(MouseEvent mouseEvent) {
+    }
+
+//    private Speler getSpeler() {
+//        return Speler; // Dit moet worden gewijzigd
+//    }
+}
