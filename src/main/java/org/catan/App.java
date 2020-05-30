b1be0f7f02a37fb0ecbed17374cd9a570bfbddf0
Jeroen <jegbol01@gmail.com>
2020-05-30 21:51:12 +0200

    fix

--- src/main/java/org/catan/App.java
+++ src/main/java/org/catan/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,12 +15,23 @@ import java.io.IOException;
 public class App extends Application {
 
     private static Scene scene;
+    private static Stage stage;
+
+    private AnchorPane screenView;
+    private AnchorPane boardView;
+    private AnchorPane stockView;
+    private AnchorPane logView;
+    private AnchorPane chatView;
 
     @Override
     public void start(Stage stage) throws IOException {
+        this.stage = stage;
+
+//        scene = new Scene(App.loadFXML("Views/screenView"));
         scene = new Scene(loadFXML("Views/mainView"));
         scene.getStylesheets().add(App.class.getResource("assets/style/style.css").toExternalForm());
         stage.setScene(scene);
+
         stage.show();
     }
 
@@ -27,7 +39,7 @@ public class App extends Application {
         scene.setRoot(loadFXML(fxml));
     }
 
-    private static Parent loadFXML(String fxml) throws IOException {
+    public static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
         return fxmlLoader.load();
     }
