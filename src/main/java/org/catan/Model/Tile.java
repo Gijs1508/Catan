ac22325873f4a590452987056023ec2b7b368b80
Jan <s1120303@student.hsleiden.nl>
2020-06-01 11:37:40 +0200

    Created methods in spellbord

--- src/main/java/org/catan/Model/Tile.java
+++ src/main/java/org/catan/Model/Tile.java
@@ -1,4 +1,17 @@
 package org.catan.Model;
 
 public class Tile {
+    private String type;
+    private String amount;
+
+
+    public Tile(){
+
+    }
+
+    public String getType() {
+        return this.type;
+    }
+
+
 }
