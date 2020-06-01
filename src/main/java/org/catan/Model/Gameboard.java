9b1656349d5916275a2b304b3d59cd78acdcafbf
Jan <s1120303@student.hsleiden.nl>
2020-06-01 12:12:39 +0200

    :pencil2:  Updated language > Gameboard

---
+++ src/main/java/org/catan/Model/Gameboard.java
@@ -0,0 +1,39 @@
+package org.catan.Model;
+
+import java.util.HashMap;
+
+public class Gameboard {
+    private HashMap tiles;
+    private HashMap settlements;
+    private HashMap players;
+    //    private Thief thief; todo
+    private HashMap harbour;
+
+    public Gameboard() {
+
+    }
+
+    public void setTiles() {
+
+    }
+
+    public void setSettlements(Speler speler, Tile tile) {
+
+    }
+
+    public void setThief(Speler speler, Tile tile) {
+
+    }
+
+    public void setHarbour() {
+
+    }
+
+    public HashMap getTiles() {
+        return this.tiles;
+    }
+
+    public HashMap getSettlements() {
+        return this.settlements;
+    }
+}
