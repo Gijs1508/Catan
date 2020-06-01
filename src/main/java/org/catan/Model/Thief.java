022afdbc548c08e0d0283e57092763e301163f98
Jan <s1120303@student.hsleiden.nl>
2020-06-01 13:15:37 +0200

    Added classes

--- src/main/java/org/catan/Model/Thief.java
+++ src/main/java/org/catan/Model/Thief.java
@@ -2,21 +2,12 @@ package org.catan.Model;
 
 public class Thief {
     private Tile tile;
-    private Speler speler;
 
     public void setTile(Tile tile) {
         this.tile= tile;
     }
 
-    public void setSpeler(Speler speler) {
-        this.speler = speler;
-    }
-
     private Tile getTile() {
         return this.tile;
-    }
-
-    private Speler getSpeler() {
-        return this.speler;
     }
 }
