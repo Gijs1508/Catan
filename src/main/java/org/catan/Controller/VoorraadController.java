060ac3bf1763300490984cc6a03a4ae9e4034573
Jeroen <jegbol01@gmail.com>
2020-06-03 19:01:41 +0200

    added card animation

--- src/main/java/org/catan/Controller/VoorraadController.java
+++ src/main/java/org/catan/Controller/VoorraadController.java
@@ -1,17 +1,72 @@
 package org.catan.Controller;
 
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
 
-public class VoorraadController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class VoorraadController implements Initializable {
 
     @FXML private ImageView knightCard;
+    @FXML private ImageView animationKnightCard;
 
-    LogController logController = LogController.getInstance();
+    private AnimationTimer animationTimer;
+    private boolean animationIsActive = false;
+
+
+    private LogController logController = LogController.getInstance();
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+    }
 
     @FXML
     public void activateKnight() {
         logController.logKnightEvent();
+
+        removeCardAnimation(animationKnightCard);
+    }
+
+    private void removeCardAnimation(ImageView animationCard){
+        animationCard.setVisible(true);
+
+        double x = animationCard.getTranslateX();
+        double y = animationCard.getTranslateY();
+
+        if(!animationIsActive){
+            animationTimer = new AnimationTimer() {
+                int tick = 0;
+                @Override
+                public void handle(long l) {
+                    tick++;
+                    animationIsActive = true;
+
+                    animationCard.setTranslateY(animationCard.getTranslateY() - 4);
+
+                    if(tick > 40) {
+                        animationCard.setOpacity(animationCard.getOpacity() - 0.04);
+                    }
+
+                    if(animationCard.getOpacity() <= 0) {
+                        animationTimer.stop();
+
+                        animationCard.setTranslateX(x);
+                        animationCard.setTranslateY(y);
+                        animationCard.setOpacity(1);
+                        animationCard.setVisible(false);
+
+                        tick = 0;
+                        animationIsActive = false;
+                    }
+                }
+            };
+            animationTimer.start();
+        }
     }
 
     @FXML
