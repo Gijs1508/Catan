05c39714170f1aac8bbf73a9d984052f5708e8b8
Sabrina <sab.shine.art@gmail.com>
2020-06-04 11:46:30 +0200

    Thief class aangepast

--- src/main/java/org/catan/Model/Thief.java
+++ src/main/java/org/catan/Model/Thief.java
@@ -2,6 +2,27 @@ package org.catan.Model;
 
 public class Thief {
     private Tile tile;
+//    private double x;
+//    private double y;
+    private String imgPath;
+
+    public Thief(Tile tile) {
+//        this.x = x;
+//        this.y = y;
+        this.imgPath = "assets/img/gameobjects/Thief.png";
+    }
+
+//    public double getX() {
+//        return this.x;
+//    }
+//
+//    public double getY() {
+//        return this.y;
+//    }
+
+    public String getImgPath() {
+        return this.imgPath;
+    }
 
     public void setTile(Tile tile) {
         this.tile= tile;
