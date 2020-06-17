8e9424df16c015adddc7ad6d9ec41a0003f36ccf
Jeroen <jegbol01@gmail.com>
2020-06-17 23:07:45 +0200

    Update TurnManager.java

--- src/main/java/org/catan/Model/TurnManager.java
+++ src/main/java/org/catan/Model/TurnManager.java
@@ -14,16 +14,16 @@ public class TurnManager {
         ArrayList<Player> allPlayers = App.getCurrentGame().getPlayers();
         for (Player player : allPlayers){
             if (player.equals(lastPlayer)){
-                if (allPlayers.indexOf(lastPlayer) == (allPlayers.size() - 1)){
+//                if (allPlayers.indexOf(lastPlayer) == (allPlayers.size() - 1)){
+                if (turn - 1 == (allPlayers.size())){ // TODO keeps repeating the same player every few times
                     turn = 0;
                 } else {
                     turn++;
                 }
-
                 System.out.println(lastPlayer.getName() + " ended their turn. " + allPlayers.get(turn).getName() + "'s turn is up next.");
 
                 lastPlayer.setTurn(false);
-                App.getCurrentGame().getPlayers().get(turn).setTurn(true);
+                allPlayers.get(turn).setTurn(true);
 
                 if(App.getClientPlayer().isTurn()) {
                     Sound.playStartTurnJingle();
