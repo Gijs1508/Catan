7081e07b2e7a6d322593100401bed848303559f3
Jan <s1120303@student.hsleiden.nl>
2020-06-18 21:33:45 +0200

    Commented thread added update function

--- src/main/java/org/catan/Controller/StartPhaseController.java
+++ src/main/java/org/catan/Controller/StartPhaseController.java
@@ -1,39 +1,54 @@
 package org.catan.Controller;
+import javafx.fxml.Initializable;
 import org.catan.App;
+import org.catan.Model.Game;
 import org.catan.Model.Player;
 import org.catan.Model.TurnManager;
+import org.catan.interfaces.Observable;
 
-public class StartPhaseController implements Runnable {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class StartPhaseController implements Observable {
     private static int startPhaseCount;
     //TODO Adding to the database if possible
     // So that a log can be displayed when startPhase has ended
     private static int startPhaseCountGlobal;
     private static boolean startPhaseActive = true;
+    private static boolean waitState = true;
 
-    public void run() {
-        while(true) {
-            if (App.getClientPlayer().isTurn()) {
-                try {
-                    Thread.sleep(1000);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                activateBuildingStartPhase();
-                try {
-                    Thread.sleep(8000);
-//                    wait(startPhaseCount);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            } else if(!startPhaseActive) {
-                break;
-            }
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException ei) {
-                System.out.println("Im doing sleepy time");
-            }
-        }
+//    public void run() {
+//        while(true) {
+//            if (App.getClientPlayer().isTurn()) {
+//                try {
+//                    Thread.sleep(1000);
+//                } catch (Exception e) {}
+//                activateBuildingStartPhase();
+//                while(waitState) {
+//                    System.out.println("I am looping");
+//                    try {
+//                        System.out.println("sleepy time");
+//                        Thread.sleep(500);
+//                    } catch (Exception e){}
+//
+//                    // Can't use wait because then we need to multithread the program
+//                }
+//                try {
+//                    Thread.sleep(3000);
+//                } catch (Exception e) {}
+//            } else if(!startPhaseActive) {
+//                break;
+//            }
+//            try {
+//                Thread.sleep(3000);
+//            } catch (InterruptedException ei) {}
+//            waitState = true;
+//        }
+//    }
+
+    public static void setWaitState(boolean waitState1) {
+        waitState = waitState1;
     }
 
     public static boolean isStartPhaseActive() {
@@ -54,7 +69,16 @@ public class StartPhaseController implements Runnable {
     }
 
     public void activateBuildingStartPhase() {
+        System.out.println("OOMGGG");
         if (startPhaseActive)
             GameSchermController.getInstance().villageStartPhase();
     }
+
+    @Override
+    public void update(Game game) throws IOException {
+        if(game.turnPlayerGetter().equals(App.getClientPlayer())){
+            activateBuildingStartPhase();
+        }
+    }
+
 }
