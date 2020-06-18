b783aeaf91c3cd00de770bd606f65fa284be7fa5
Jan <s1120303@student.hsleiden.nl>
2020-06-18 19:49:44 +0200

    Made a run method for starting startPhase when isTurn == true

--- src/main/java/org/catan/Controller/StartPhaseController.java
+++ src/main/java/org/catan/Controller/StartPhaseController.java
@@ -3,12 +3,38 @@ import org.catan.App;
 import org.catan.Model.Player;
 import org.catan.Model.TurnManager;
 
-public class StartPhaseController {
+public class StartPhaseController implements Runnable {
     private static int startPhaseCount;
     //TODO Adding to the database if possible
     // So that a log can be displayed when startPhase has ended
     private static int startPhaseCountGlobal;
     private static boolean startPhaseActive = true;
+
+    public void run() {
+        while(true) {
+            if (App.getClientPlayer().isTurn()) {
+                try {
+                    Thread.sleep(1000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                activateBuildingStartPhase();
+                try {
+                    Thread.sleep(8000);
+//                    wait(startPhaseCount);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            } else if(!startPhaseActive) {
+                break;
+            }
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException ei) {
+                System.out.println("Im doing sleepy time");
+            }
+        }
+    }
 
     public static boolean isStartPhaseActive() {
         return startPhaseActive;
