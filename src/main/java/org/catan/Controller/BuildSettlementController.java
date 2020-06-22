4e050ef4e2a53ac41273f75f9aa6a0e5f4d2f2bb
Jan <s1120303@student.hsleiden.nl>
2020-06-22 11:26:52 +0200

    Removed useless code and todo

--- src/main/java/org/catan/Controller/BuildSettlementController.java
+++ src/main/java/org/catan/Controller/BuildSettlementController.java
@@ -293,20 +293,8 @@ public class BuildSettlementController implements Observable {
         App.getCurrentGame().turnPlayerGetter().addVillagePoint();
         App.getCurrentGame().turnPlayerGetter().addVictoryPoint();
         DatabaseConnector.getInstance().updateGame(App.getCurrentGame());
-//        checkPlayerWon(App.getCurrentGame().turnPlayerGetter());
-
         return village;
     }
-
-//    /** checks if the player has won
-//     * @param turnPlayerGetter the player who just build an settlement
-//     * @author Gijs */
-//    private void checkPlayerWon(Player turnPlayerGetter) {
-//        if(turnPlayerGetter.getScore() >= 10){
-//            // TODO: Change this to a real function.
-//            ScoreController.getInstance().testGameEnd();
-//        }
-//    }
 
     /** Finds the harbor that the settlement has been placed adjacent to and updates accordingly.
      * @param node the vertex a settlement has been placed on
@@ -327,7 +315,6 @@ public class BuildSettlementController implements Observable {
             }
         }
     }
-
 
     /**
      * The methods upgrades a Village and returns it
@@ -350,7 +337,6 @@ public class BuildSettlementController implements Observable {
             App.getCurrentGame().turnPlayerGetter().removeVillagePoint();
             App.getCurrentGame().getBoard().setSettlements(buildVillages);
             DatabaseConnector.getInstance().updateGame(App.getCurrentGame());
-//            checkPlayerWon(App.getCurrentGame().turnPlayerGetter());
 
         } else {
             Alert alert = new Alert(Alert.AlertType.WARNING);
@@ -459,19 +445,6 @@ public class BuildSettlementController implements Observable {
             App.getCurrentGame().getBoard().setSettlements(buildVillages);
         }
     }
-
-    // Keep this for if updateSettlements is updating to much
-//    private boolean isSettlementArrayTheSame(ArrayList<Village> villages) {
-//        if (villages.size() > buildVillages.size())
-//            return true;
-//        else {
-//            for (int i=0; i < villages.size(); i++) {
-//                if (buildVillages.get(i).isUpgraded() != villages.get(i).isUpgraded())
-//                    return true;
-//            }
-//        }
-//        return false;
-//    }
 
     // returns the instance of this class
     public static BuildSettlementController getInstance() {
