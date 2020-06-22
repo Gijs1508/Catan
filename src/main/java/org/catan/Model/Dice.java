a8c686818521c976d01e04866cde8dffac14c637
Gijs1508 <s1120339@student.hsleiden.nl>
2020-06-22 11:24:14 +0200

    created resourcescontroller

--- src/main/java/org/catan/Model/Dice.java
+++ src/main/java/org/catan/Model/Dice.java
@@ -1,11 +1,9 @@
 package org.catan.Model;
 
 import org.catan.App;
-import org.catan.Controller.GameSchermController;
-import org.catan.Controller.LogController;
-import org.catan.Controller.ScreenController;
-import org.catan.Controller.ThiefController;
+import org.catan.Controller.*;
 import org.catan.Helper.BuildVillages;
+import org.catan.logic.DatabaseConnector;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -40,55 +38,8 @@ public class Dice {
 
         LogController.getInstance().logRollEvent(Integer.toString(dice1), Integer.toString(dice2));
 
-        setPlayerResources(total);
+        ResourcesController.getInstance().setPlayerResources(total);
         return diceResult;
     }
 
-    private void setPlayerResources(int total){
-        ArrayList<Tile> usedTiles = new ArrayList<>();
-        if(App.getCurrentGame().getBoard().getSettlements() != null) {
-            for (Village village : App.getCurrentGame().getBoard().getSettlements()) {
-                for (Tile tile : village.getConnectedTiles()){
-                    int amount;
-                    if(village.isUpgraded()){
-                        amount = 2;
-                    } else {
-                        amount = 1;
-                    }
-                    if(total == tile.getNumber() && !usedTiles.contains(tile)){
-                        addResourcesToPlayer(App.getClientPlayer().getColor(), tile, amount);
-                        usedTiles.add(tile);
-                    }
-                }
-            }
-        }
-    }
-
-
-    private void addResourcesToPlayer(String playerColor, Tile tile, int amount){
-        ArrayList<Village> usedVillages = new ArrayList<>();
-        ArrayList<String> receivedResources = new ArrayList<>();
-
-        System.out.println("==============================");
-        for(Village vill : App.getCurrentGame().getBoard().getSettlements()){
-            System.out.println("villagekleur + playerkleur: " + vill.getColor() + "\t" + playerColor);
-            if(vill.getColor().equals(playerColor) && !usedVillages.contains(vill)){
-                System.out.println("ik geef je " + tile.getType());
-                System.out.println();
-                System.out.println(vill.getColor() + "\t" + vill.getX() + "\t" + vill.getY());
-
-                App.getClientPlayer().getPlayerInventory().changeCards(tile.getType(), amount);
-                receivedResources.add(tile.getType());
-                usedVillages.add(vill);
-            }
-        }
-        System.out.println("==============================");
-
-
-        if(!receivedResources.isEmpty()) {
-            LogController.getInstance().logReceiveEvent(receivedResources);
-        }
-
-
-    }
 }
