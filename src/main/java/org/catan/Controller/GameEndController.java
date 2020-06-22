247b2126789dffd81ba9c9f84eb5cbe9784fbb71
Jan <s1120303@student.hsleiden.nl>
2020-06-22 10:36:12 +0200

    Removed the todo

--- src/main/java/org/catan/Controller/GameEndController.java
+++ src/main/java/org/catan/Controller/GameEndController.java
@@ -69,7 +69,6 @@ public class GameEndController implements Observable {
         return gameEndController;
     }
 
-    // TODO Updating the scores to the database
     @Override
     public void update(Game game) throws IOException {
         if (didSomeoneWin()) {
